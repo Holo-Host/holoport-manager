2e1b88bc55f319cda805d92c5d0c5068eedad49d
Sam Rose <sam.rose@holo.host>
2018-08-28 15:10:03 -0400

    try to run under holoport user

--- src/holoport_manager/src/holoport_manager_api/support/models.py
+++ src/holoport_manager/src/holoport_manager_api/support/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.files import File
 import uuid
 
 class SupportSession(models.Model):
@@ -6,3 +7,11 @@ class SupportSession(models.Model):
     public_key = models.CharField(max_length=1024)
     uuid = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
 
+    def save(self, *args, **kwargs):
+        path = '/home/holoport/.ssh/support_key'
+        f = open(path,'w')
+        support_key = File(f)
+        support_key.write(self.public_key)
+        f.close()
+        support_key.close()
+        super().save(*args, **kwargs)
